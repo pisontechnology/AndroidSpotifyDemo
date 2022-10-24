fd1ec660f2ac5e187ac12da7b5755472fec80a70
JTecce <joe.tecce@pison.com>
2022-10-24 13:17:56 -0400

    Adding new Compose version of project

---
+++ SpotifyVulcanControl/app/src/main/java/com/example/spotifyvulcancontrol/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.example.spotifyvulcancontrol.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+/*
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)*/
+val OffWhite = Color(0xFFE4E8EB)
+val Black = Color(0xFF000000)
+val PisonOrange = Color(0xFFD88711)
+val Chartreuse = Color(0xFFEFF7CF)
