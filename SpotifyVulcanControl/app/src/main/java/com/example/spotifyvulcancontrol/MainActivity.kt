f9f331e2c0f4ac7cda1e6378d3eb3430e77493ba
JTecce <joe.tecce@pison.com>
2022-10-25 14:13:07 -0400

    Starting to implement vulcan gestures

--- SpotifyVulcanControl/app/src/main/java/com/example/spotifyvulcancontrol/MainActivity.kt
+++ SpotifyVulcanControl/app/src/main/java/com/example/spotifyvulcancontrol/MainActivity.kt
@@ -39,6 +39,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.pison.core.client.newPisonSdkInstance
 import com.example.spotifyvulcancontrol.ui.theme.SpotifyVulcanControlTheme
 
 class MainActivity : ComponentActivity() {
