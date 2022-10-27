f9ff03d71c93e2a7d70242a740b8740e5fdd5c24
JTecce <joe.tecce@pison.com>
2022-10-27 11:29:20 -0400

    SSVA-544 Adding spotify integration
    Also started the integration of old project

---
+++ SpotifyVulcanControl/app/src/main/java/com/example/spotifyvulcancontrol/DeviceService.kt
@@ -0,0 +1,416 @@
+package com.example.spotifyvulcancontrol
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.badoo.reaktive.disposable.CompositeDisposable
+import com.badoo.reaktive.observable.flatMapIterable
+import com.badoo.reaktive.observable.observeOn
+import com.badoo.reaktive.observable.subscribe
+import com.badoo.reaktive.scheduler.mainScheduler
+import com.pison.core.client.PisonRemoteDevice
+import com.pison.core.client.PisonRemoteClassifiedDevice
+import com.pison.core.client.monitorConnectedDevices
+import com.pison.core.shared.haptic.*
+import com.pison.core.shared.imu.EulerAngles
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import com.spotify.protocol.types.Track;
+import kotlinx.coroutines.Runnable
+
+const val SERVER_INITIALIZE_COMMAND = 1
+private const val TAG = "Device Service"
+private const val EULER_TAG = "EULER"
+private const val GESTURES_TAG = "GESTURES"
+
+private const val HAPTICCT = 5
+private const val HAPTIC_PULSE = 2
+private const val HAPTIC_BURST = 3
+
+private const val DURATION_MS_DEFAULT = 245
+private const val INTENSITY_UnlockLock = 100
+private const val INTENSITY_BASIC = 70
+private const val NUMBER_DEFAULT_UnlockLock = 2
+private const val NUMBER_DEFAULT_BASIC = 1
+
+@Suppress("DEPRECATION")
+@ExperimentalStdlibApi
+class DeviceService: Service(){
+    private var pisonRemoteDevice: PisonRemoteDevice? = null
+
+    private var masterDisposable = CompositeDisposable()
+
+    lateinit var audioManager: AudioManager
+
+    lateinit var runnable: Runnable
+
+    val mHandler = Handler(Looper.getMainLooper())
+
+    private val _eulerReceived = MutableLiveData<EulerAngles>()
+    val eulerReceived: LiveData<EulerAngles>
+        get() = _eulerReceived
+
+    private val _errorReceived = MutableLiveData<Throwable>()
+    val errorReceived: LiveData<Throwable>
+        get() = _errorReceived
+
+    private var wakeword = false
+    private var isPlaying = false
+    private var isUpward = false
+    private var isDownward = false
+    private var isIndexed = false
+    private var debounce = false
+    var swipedUp = false
+    var swipedDown = false
+
+    companion object {
+        private const val ACTION_START_SERVICE = "action_start_service"
+        private const val ACTION_STOP_SERVICE = "action_stop_service"
+
+        fun getStartIntent(context: Context): Intent {
+            return with(Intent(context, DeviceService::class.java)) {
+                action = ACTION_START_SERVICE
+                println("INTENT STARTING*************")
+                this
+            }
+        }
+
+        fun getStopIntent(context: Context): Intent {
+            return with(Intent(context, DeviceService::class.java)) {
+                action = ACTION_STOP_SERVICE
+                println("STOPPING INTENT****************")
+                this
+            }
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        audioManager = applicationContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        println("Am I being created")
+
+        moniter()
+
+        // checks every 1.2sec to see if the user is holding a swipe up or swipe down gesture
+        // if so will gradually change volume based off of that information
+        mHandler.post(object: Runnable{
+            override fun run() {
+                //println("am I called?")
+                println(swipedUp)
+                if(swipedUp){
+                    println("Steady increase volume")
+                    audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI)
+                }
+                else if(swipedDown){
+                    audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI)
+                }
+                mHandler.postDelayed(this,1200)
+            }
+        })
+    }
+
+    //region Initial set-up stuff
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposeDisposables()
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    private fun moniter(){
+        Log.d(TAG, "connect called")
+        val deviceDisposable = Application.sdk.monitorConnectedDevices().observeOn(mainScheduler).subscribe(
+            onNext = { pisonDevice ->
+                Log.d(TAG, "$TAG SUCCESS")
+                inTakeData(pisonDevice)
+                pisonRemoteDevice = pisonDevice
+
+            },
+            onError = { throwable ->
+                Log.d(TAG, "$TAG Error:$throwable")
+                _errorReceived.postValue(throwable)
+            }
+        )
+        masterDisposable.add(deviceDisposable)
+    }
+
+    private fun inTakeData(pisonRemoteDevice: PisonRemoteClassifiedDevice){
+        monitorGestures(pisonRemoteDevice)
+        monitorEuler(pisonRemoteDevice)
+        monitorImuState(pisonRemoteDevice)
+    }
+
+    private fun sendHaptic(hapticCommandCode: Int, durationMs: Int, intensity: Int, numberBursts: Int) {
+        GlobalScope.launch {
+            val haptic = getHapticCommand(
+                hapticCommandCode,
+                durationMs,
+                intensity,
+                numberBursts
+            )
+            pisonRemoteDevice?.sendHaptic(haptic)
+        }
+    }
+
+    private fun getHapticCommand(
+        hapticCommandCode: Int,
+        durationMs: Int,
+        intensity: Int,
+        number: Int
+    ): HapticCommand {
+        return when (hapticCommandCode) {
+            HAPTICCT -> HapticOnCommand(intensity)
+            HAPTIC_PULSE -> HapticPulseCommand(intensity, durationMs)
+            HAPTIC_BURST -> HapticBurstCommand(intensity, durationMs, number)
+            else -> HapticOffCommand
+        }
+    }
+
+    private fun disposeDisposables() {
+        Log.d(TAG, "disposed of all disposables")
+        masterDisposable.clear(dispose = true)
+    }
+
+    //endregion
+
+    //region monitor functions
+
+    // using IMU accelerameter data will trigger two bools to determine if ether your wrist is forwards or backwards
+    private fun monitorImuState(pisonRemoteDevice: PisonRemoteClassifiedDevice){
+        val imuStateDisposable =
+            pisonRemoteDevice.monitorImu().observeOn(mainScheduler).subscribe(
+                onNext = { imu ->
+                    if(imu.acceleration.y < -8.0f){
+                        //println(imu.acceleration.x)
+                        if(imu.acceleration.x < -0.7f){
+                            isUpward = true
+                            isDownward = false
+                        }
+                        else if(imu.acceleration.x > -0.3f){
+                            isUpward = false
+                            isDownward = true
+                        }
+                    }
+                    if(imu.acceleration.y - imu.acceleration.z > -4.0f){
+                        if(imu.acceleration.x < -4.0f){
+                            isUpward = true
+                            isDownward = false
+                        }
+                        else{ //if (imu.acceleration.x > 5.0f){
+                            isUpward = false
+                            isDownward = true
+                        }
+                    }
+                },
+                onError = { throwable ->
+                    Log.d("IMU STATES", "ERROR: $throwable")
+                }
+            )
+        masterDisposable.add(imuStateDisposable)
+    }
+
+    private fun monitorGestures(pisonRemoteDevice: PisonRemoteClassifiedDevice) {
+        val gestureDisposable =
+            pisonRemoteDevice.monitorFrameTags().flatMapIterable { it }.observeOn(mainScheduler).subscribe(
+                onNext = { gesture ->
+                    Log.d(TAG, "$GESTURES_TAG $gesture")
+                    //print(gesture)
+                    if (gesture == "SHAKE_N_INEH"){
+                        wakeword = !wakeword
+                        if(wakeword){
+                            println("WOKE UP")
+                            debounce = true
+                            sendHaptic(
+                                HAPTIC_BURST,
+                                DURATION_MS_DEFAULT,
+                                INTENSITY_UnlockLock,
+                                NUMBER_DEFAULT_UnlockLock
+                            )
+                        }else{
+                            println("NAP TIME")
+                            debounce = false
+                            isIndexed = false
+                            sendHaptic(
+                                HAPTIC_BURST,
+                                DURATION_MS_DEFAULT,
+                                INTENSITY_UnlockLock,
+                                NUMBER_DEFAULT_UnlockLock
+                            )
+                        }
+                    }
+                    if (wakeword) {
+                        if(gesture == "DEBOUNCE_LDA_INEH"){
+                            isIndexed = true // trigger user is Indexing at the moment
+                        }
+
+                        if(gesture == "DEBOUNCE_LDA_TEH" && isUpward && !debounce){
+                            // using imu data with thumb can figure out its a thumbs up
+                            isIndexed = false
+                            debounce = true
+                            println("Liked Song")
+
+                            // Adding song to liked songs
+                            Application.spotifyAppRemote.playerApi.playerState.setResultCallback {
+                                if(it.track.name != null){
+                                    Application.spotifyAppRemote.userApi.addToLibrary(it.track.uri)
+                                }
+                            }
+
+                            sendHaptic(
+                                HAPTIC_BURST,
+                                DURATION_MS_DEFAULT,
+                                INTENSITY_BASIC,
+                                NUMBER_DEFAULT_BASIC
+                            )
+                        }
+                        else if(gesture == "DEBOUNCE_LDA_TEH" && isDownward && !debounce){
+                            // using imu data with thumb can figure out its a thumbs down
+                            isIndexed = false
+                            debounce = true
+                            println("Unliked Song")
+
+                            // removing song from liked songs
+                            Application.spotifyAppRemote.playerApi.playerState.setResultCallback {
+                                if(it.track.name != null){
+                                    Application.spotifyAppRemote.userApi.removeFromLibrary(it.track.uri)
+                                }
+                            }
+
+                            sendHaptic(
+                                HAPTIC_BURST,
+                                DURATION_MS_DEFAULT,
+                                INTENSITY_BASIC,
+                                NUMBER_DEFAULT_BASIC
+                            )
+                        }
+                        else if(gesture == "INEH_SWIPE_RIGHT"){
+                            // skip to next song
+                            isIndexed = false
+                            debounce = true
+                            println("Play Next Song")
+                            Application.spotifyAppRemote.playerApi.skipNext()
+                        }
+                        else if(gesture == "INEH_SWIPE_LEFT"){
+                            // skip to previous song
+                            isIndexed = false
+                            debounce = true
+                            println("Play Prev Song")
+                            Application.spotifyAppRemote.playerApi.skipPrevious()
+                        }
+                        else if(gesture == "INEH_SWIPE_UP"){
+                            // Swipe up will increase volume set amount
+                            debounce = true
+                            swipedUp = true // trigger hold functionality
+                            println("Increase Volume")
+                            audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND)
+                        }
+                        else if(gesture == "INEH_SWIPE_DOWN"){
+                            // Swipe down will decrease volume set amount
+                            debounce = true
+                            swipedDown = true // trigger hold functionality
+                            println("Decrease Volume")
+                            audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI)
+                        }
+                        /*else if(gesture == "DEBOUNCE_LDA_FHEH"){
+                            isIndexed = false
+                            debounce = true
+                            println("Shuffle Playlist")
+                            Application.spotifyAppRemote.playerApi.toggleShuffle()
+                        }*/ // TODO: Possible way to add shuffling (just find a way to get current Shuffle State)
+                        else if(gesture != "DEBOUNCE_LDA_INEH" && isIndexed &&
+                            !debounce && !swipedDown && !swipedUp){
+                            // User closed hand without preforming any other gesture
+                            isIndexed = false
+                            //println("CLICKED")
+
+                            // Check if spotify is currently playing or paused and trigger opposite
+                            Application.spotifyAppRemote.playerApi.playerState.setResultCallback {
+                                //Log.d(TAG, "isPaused = " + it.isPaused)
+                                if(it.isPaused){
+                                    isPlaying = false
+                                }
+                                else{
+                                    isPlaying = true
+                                }
+                            }
+
+                            // Call oposite based off of previous information
+                            Handler(Looper.getMainLooper()).postDelayed({
+                                if(!isPlaying){
+                                    println("Play Song")
+                                    Application.spotifyAppRemote.playerApi.resume()
+                                }
+                                else{
+                                    println("Pause Song")
+                                    Application.spotifyAppRemote.playerApi.pause()
+                                }
+
+                                sendHaptic(
+                                    HAPTIC_BURST,
+                                    DURATION_MS_DEFAULT,
+                                    INTENSITY_BASIC,
+                                    NUMBER_DEFAULT_BASIC
+                                )
+                            }, 70)
+                        }
+                        else if(gesture == "DEBOUNCE_LDA_INEH" && swipedUp ||
+                            gesture == "DEBOUNCE_LDA_INEH" && swipedDown){
+                            mHandler.post(object: Runnable{
+                                override fun run() {
+                                    mHandler.postDelayed({
+                                        if(swipedUp){
+                                            println("Steady increase volume")
+                                            audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI)
+                                        }
+
+                                        if(swipedDown){
+                                            audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI)
+                                        }
+                                    }, 500)
+                                }
+                            })
+                        }
+                        else if(gesture == "DEBOUNCE_LDA_INACTIVE" && debounce){
+                            // when hand is at rest reset everything
+                            debounce = false
+                            isIndexed = false
+                            swipedDown = false
+                            swipedUp = false
+                        }
+                    }
+                },
+                onError = { throwable ->
+                    Log.d(TAG, "ERROR $GESTURES_TAG $throwable")
+                    _errorReceived.postValue(throwable)
+                }
+            )
+        masterDisposable.add(gestureDisposable)
+    }
+
+    private fun monitorEuler(pisonRemoteDevice: PisonRemoteClassifiedDevice) {
+        val eulerDisposable =
+            pisonRemoteDevice.monitorEulerAngles().observeOn(mainScheduler).subscribe(
+                onNext = { euler ->
+                    //Log.d(TAG, "$EULER_TAG $euler")
+                    _eulerReceived.postValue(euler)
+                },
+                onError = { throwable ->
+                    Log.d(TAG, " $EULER_TAG ERROR $throwable")
+                    _errorReceived.postValue(throwable)
+                }
+            )
+        masterDisposable.add(eulerDisposable)
+    }
+
+    //endregion
+}
--- SpotifyVulcanControl/app/src/main/java/com/example/spotifyvulcancontrol/MainActivity.kt
+++ SpotifyVulcanControl/app/src/main/java/com/example/spotifyvulcancontrol/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.spotifyvulcancontrol
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import android.os.Bundle
+import android.media.AudioManager
+import android.os.*
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
@@ -39,10 +45,24 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.pison.core.client.newPisonSdkInstance
 import com.example.spotifyvulcancontrol.ui.theme.SpotifyVulcanControlTheme
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.example.spotifyvulcancontrol.Application.Companion.spotifyAppRemote
+import com.spotify.android.appremote.api.SpotifyAppRemote
+
+private const val PISON_PACKAGE = "com.example.spotifyvulcancontrol"
+private const val PISON_CLASS = "$PISON_PACKAGE.DeviceService"
 
 class MainActivity : ComponentActivity() {
+
+    private val clientId = "fc1ec90c567f4687b700ec8f0f237871"
+    private val redirectUri = "https://com.example.spotifyvulcancontrol/callback"
+
+    private var spotifyConnected = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -51,7 +71,69 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        startService() // Set up background service
+
+        // Set the connection parameters
+        val connectionParams = ConnectionParams.Builder(clientId)
+            .setRedirectUri(redirectUri)
+            .showAuthView(true)
+            .build()
+
+        val listener = object : Connector.ConnectionListener{
+            override fun onConnected(appRemote: SpotifyAppRemote) {
+                spotifyAppRemote = appRemote
+                Log.d("MainActivity", "Connected! Yay!")
+                // Now you can start interacting with App Remote
+                connected()
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                Log.e("MainActivity", throwable.message, throwable)
+                // Something went wrong when attempting to connect! Handle errors here
+            }
+        }
+
+        SpotifyAppRemote.connect(this, connectionParams, listener)
+    }
+
+    private fun connected(){
+        // just called when we have a success ful connection will be useful for UI stuff
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun startService(){
+        val startIntent = DeviceService.getStartIntent(this)
+        ContextCompat.startForegroundService(this, startIntent)
+        startService(startIntent)
+
+        val bindIntent = Intent().apply {
+            component = ComponentName(PISON_PACKAGE, PISON_CLASS)
+        }
+        bindService(bindIntent, connection, Context.BIND_AUTO_CREATE)
+    }
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, serviceBind: IBinder) {
+            val service = Messenger(serviceBind)
+            val message = Message.obtain(null, SERVER_INITIALIZE_COMMAND)
+            service.send(message)
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            TODO("Not yet implemented")
+        }
+    }
 }
+
+//region Compose Functions
 
 @Composable
 private fun MyApp() {
@@ -487,20 +569,15 @@ private fun CardContent(name: String) {
     }
 }
 
-
-/*@Preview(
-    showBackground = true,
-    widthDp = 320,
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "DefaultPreviewDark"
-)*/
+// Preview for EMI stuff
+/*
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     SpotifyVulcanControlTheme {
         RealTimePortrait()//eulerStream, waveProcessor, ldaOutput, eventOutput, swipeOutput, sqiOutput, rssiOutput)
     }
-}
+}*/
 
 /* For later
 class RealTimeVizViewModelAdapter(delegate: RealTimeVizViewModel) :
@@ -535,3 +612,5 @@ fun OnboardingPreview() {
         OnboardingScreen(onContinueClicked = {})
     }
 }
+
+//endregion
